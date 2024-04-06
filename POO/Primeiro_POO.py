009f48fbe87f0db1bc5f28d6ca14c5c0cd89b600
Ruann Campos <ruann.campos01@gmail.com>
2024-04-06 09:42:17 -0300

    Update Primeiro_POO.py

--- POO/Primeiro_POO.py
+++ POO/Primeiro_POO.py
@@ -14,4 +14,23 @@ class Bicicleta:
     def correr(self):
         print('Vruum...')
 
+    def get_cor(self):
+        return self.cor
+
+    #def __str__(self):
+    #    return f'Bicicleta : {self.cor}, {self.modelo}, {self.ano}, {self.valor}'
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}: {''.join([f'{chave} = {valor}'  for chave, valor in self.__dict__.items()])}'
+
+
 b1 = Bicicleta('vermelha','caloi',2022,600)
+#b1.buzinar()
+#b1.correr()
+#b1.parar()
+#print(b1.cor,b1.modelo,b1.ano,b1.valor)
+
+b2 = Bicicleta('verde','monark',2000,189)
+print(b2)
+# b2.buzinar() = Bicicleta.buzinar(b2)
+
