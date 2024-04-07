e14e504987997dd5d301ce537cf4baa6ef706bc3
Ruann Campos <ruann.campos01@gmail.com>
2024-04-07 10:50:11 -0300

    subindo exercícios

--- POO/propriedades.py
+++ POO/propriedades.py
@@ -5,14 +5,19 @@ class Foo:
     @property
     def x(self):
         return self._x or 0
-    
+
     @x.setter
     def x(self, value):
         self._x += value
 
-    
+    @x.deleter
+    def x(self):
+        self._x = 0
+
 
 foo = Foo(10)
+print(foo.x)
+del foo.x
 print(foo.x)
 foo.x = 10
 print(foo.x)
