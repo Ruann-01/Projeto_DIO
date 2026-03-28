a7c79da6ec24b209f378457a62e7ddfb51cc79ac
Ruann Campos <ruann.campos01@gmail.com>
2026-03-28 20:38:03 -0300

    Construindo a API

--- API_FAST/main.py
+++ API_FAST/main.py
@@ -1,1 +1,29 @@
 # Autor: Ruann Campos
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Rota inicial
+@app.route("/")
+def home():
+    return "API funcionando!"
+
+# Rota que retorna dados
+@app.route("/dados")
+def dados():
+    return jsonify({
+        "nome": "Ruann",
+        "curso": "Engenharia da Computação",
+        "status": "Ativo"
+    })
+
+# Rota com parâmetro
+@app.route("/saudacao/<nome>")
+def saudacao(nome):
+    return jsonify({
+        "mensagem": f"Olá, {nome}!"
+    })
+
+# Executar a API
+if __name__ == "__main__":
+    app.run(debug=True)
